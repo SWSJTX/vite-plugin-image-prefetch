9b2e2cf782403ee571741f29fe8c9a1939e927f1
SWSJTX <icenode@163.com>
2025-02-04 23:55:30 +0800

    feat: add support of menu selection

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,53 @@
 import type { Plugin, ResolvedConfig } from "vite";
 import FastGlob from "fast-glob";
 
+/**
+ * file mode enum
+ */
+export enum EMenuMode {
+  /**
+   * public menu
+   */
+  PUBLIC = 0,
+  /**
+   * src menu
+   */
+  SRC = 1
+}
+
+/**
+ * add attrs rel enum
+ */
+export enum EAttrRel {
+  /**
+   * prefetch
+   */
+  PREFETCH = "prefetch",
+  /**
+   * preload
+   */
+  PRELOAD = "preload"
+}
+
 interface IPrefetchImagesOptions {
   /**
    * directory
    */
   dir: string;
   /**
+   * file mode
+   */
+  menuMode: EMenuMode;
+  /**
    * attrs
    */
   attrs?: {
-    rel?: "prefetch" | "preload";
+    rel?: EAttrRel;
   };
 }
 
 export const vitePluginPrefetchImages = (options: IPrefetchImagesOptions): Plugin => {
-  const { dir, attrs = {} } = options;
+  const { dir, menuMode = EMenuMode.PUBLIC, attrs = {} } = options;
 
   if (!dir) {
     throw new Error("Directory not found.");
@@ -27,18 +59,40 @@ export const vitePluginPrefetchImages = (options: IPrefetchImagesOptions): Plugi
   const PUBLIC_DIR = "public";
   const BASE = "/";
 
+  const assetsImages: string[] = [];
+
   return {
     name: "vite-plugin-image-prefetch",
     configResolved(resolvedConfig: ResolvedConfig) {
       base = resolvedConfig.base;
       publicDir = resolvedConfig.publicDir;
     },
-    transformIndexHtml() {
-      const files = FastGlob.sync(dir, {
-        cwd: publicDir ?? PUBLIC_DIR
-      });
+    generateBundle(_, bundle) {
+      const bundleValues = Object.values(bundle);
+      const files = FastGlob.sync(dir);
 
-      const images = files.map(file => (base ?? BASE) + file);
+      bundleValues.forEach(bundle => {
+        if (files.includes(Reflect.get(bundle, "originalFileName"))) {
+          assetsImages.push(bundle.fileName);
+        }
+      });
+    },
+    transformIndexHtml() {
+      let images: string[] = [];
+
+      // if menuMode is public
+      if (menuMode === EMenuMode.PUBLIC) {
+        const files = FastGlob.sync(dir, {
+          cwd: publicDir ?? PUBLIC_DIR
+        });
+        images = files.map(file => (base ?? BASE) + file);
+        //     if menuMode is src
+      } else if (menuMode === EMenuMode.SRC) {
+        images = assetsImages.map(image => (base ?? BASE) + image);
+        //     else
+      } else {
+        throw new Error("Please input correct file mode.");
+      }
 
       return images.map(href => {
         return {
